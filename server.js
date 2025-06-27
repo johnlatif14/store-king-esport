365e7bca943766b5373e17b4f7ef2423ee5c9cdf
johnlatif2026 <youremail@example.com>
2025-06-27 04:50:04 +0300

    أول رفع

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const session = require("express-session");
-const sqlite3 = require("sqlite3").verbose();
+const sqlite3 = require("better-sqlite3");
 const nodemailer = require("nodemailer");
 const path = require('path');
 
 const app = express();
-const db = new sqlite3.Database("./data.db");
+const db = new sqlite3("./data.db");
 
 app.use(cors({ origin: "http://localhost:5500", credentials: true }));
 app.use(bodyParser.json());
